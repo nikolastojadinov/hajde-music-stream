d3a8d2a12ecc4f8665ffc4112cf513ff0758340e
nikolastojadinov <nikolastojadinov@yahoo.co.uk>
2025-11-19 12:47:47 +0000

    backend: fix library route + add PiAuth

--- backend/src/routes/library/index.ts
+++ backend/src/routes/library/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import PiAuth from '../../middleware/piAuth';
 import { getUserLibrary } from '../../handlers/library/getLibrary';
 
 const router = Router();
+router.use(PiAuth);
 router.get('/', getUserLibrary);
+
 export default router;
